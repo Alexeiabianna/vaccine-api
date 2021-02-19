62b8af4d9d43fc6d9d524f78fa8d3c5b79617db8
Alexeiabianna <alexeiabianna@gmail.com>
2021-02-19 01:47:15 -0300

    Postgres update

--- src/main/java/com/alexei/vacine/vacinecrmapi/model/Vaccination.java
+++ src/main/java/com/alexei/vacine/vacinecrmapi/model/Vaccination.java
@@ -3,7 +3,6 @@ package com.alexei.vacine.vacinecrmapi.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
